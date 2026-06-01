e567b6110f18fe5c7a116e8c5e4bdab282e88698
Ashutosh <ashutosh@Ashutoshs-MacBook-Air.local>
2026-06-01 13:39:13 +0530

    Update code

---
+++ DAY1/code1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1, n, sum = 0;
+
+    printf("Enter the value of n : ");
+    scanf("%d", &n);
+
+    while (i <= n)
+    {
+        sum = sum + i;
+        i++;
+    }
+    printf("The sum of first %d natural numbers is %d\n", n, sum);
+    return 0;
+}
